af6c039c7790d23ff1765754cd591ce7e3d59e5c
Alvaro Vega <alvaro.vega@optivamedia.com>
2023-11-28 12:39:02 +0100

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Optiva Media on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
